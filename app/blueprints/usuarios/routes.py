d290aa763a50628356b858ca6d381d4daeb73710
IDGS-802-23002072 <omarcorderop25@gmail.com>
2026-03-23 01:29:23 -0600

    Registro Usuarios Funcionando

--- app/blueprints/usuarios/routes.py
+++ app/blueprints/usuarios/routes.py
@@ -1,13 +1,29 @@
-from flask import render_template
+from flask import redirect, render_template, request, url_for
 from app.blueprints.usuarios import usuarios_bp
+from app.blueprints.usuarios.form import UserForm
 from app.models.usuarios import Usuario
+from werkzeug.security import generate_password_hash
+from app.utils.database_connection import db
 
-@usuarios_bp.route('/')
+@usuarios_bp.route("/")
 def index():
     usuarios = Usuario.query.all()
-    return render_template('usuarios/index.html', usuarios=usuarios)
+    return render_template("usuarios/index.html", usuarios=usuarios)
 
-@usuarios_bp.route('/usuario/registro',methods=[ 'POST'])
+
+@usuarios_bp.route("/usuario/registro", methods=["GET", "POST"])
 def registroUser():
-    
-    return render_template('usuarios/registro_usuario.html')
+    form = UserForm(request.form)
+
+    if request.method == "POST" and form.validate():
+        user = Usuario(
+            nombre_completo=form.nombre_completo.data,
+            email=form.email.data,
+            password_hash=generate_password_hash(form.password.data),
+            rol=form.rol.data,
+            estatus=form.estatus.data,
+        )
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('usuarios.index'))
+    return render_template("usuarios/registro_usuario.html", form=form)
